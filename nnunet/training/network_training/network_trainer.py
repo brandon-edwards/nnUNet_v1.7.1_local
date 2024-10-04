a1eca46331519b73981b095ad7feb96569ac4846
Edwards, Brandon <brandon.edwards@intel.com>
2024-10-04 08:58:33 -0700

    inserting debug statement

--- nnunet/training/network_training/network_trainer.py
+++ nnunet/training/network_training/network_trainer.py
@@ -199,6 +199,8 @@ class NetworkTrainer(object):
             ax = fig.add_subplot(111)
             ax2 = ax.twinx()
 
+            print(f"Brandon DEBUG, trying to plot at epochs {list(range(self.epoch + 1))} and all_tr_losses:{all_tr_losses} and all_val_losses:{all_val_losses}}")
+
             x_values = list(range(self.epoch + 1))
 
             ax.plot(x_values, self.all_tr_losses, color='b', ls='-', label="loss_tr")
